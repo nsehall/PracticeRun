55f382b16323d3dac5f89d84cb722ceba1ec9072
malevolentness <malevolentness@gmail.com>
2014-09-30 19:28:30 +1000

    added currency
    it was extremely technical

---
+++ PracticeRun/Assets/Scripts/Currency.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Currency : MonoBehaviour {
+
+    public int currency = 100;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void PlusCurrency (int amount) {
+		currency = currency + amount;
+	}
+
+	public void MinusCurrency (int amount) {
+		currency = currency - amount;
+	}
+
+}

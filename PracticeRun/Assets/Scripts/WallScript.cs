7b5a4165166bcadad1603151bf7bf6ffaa41ccdb
malevolentness <malevolentness@gmail.com>
2014-09-30 15:34:38 +1000

    added walls
    wallz

---
+++ PracticeRun/Assets/Scripts/WallScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+	
+	public int health = 100;
+	public bool occupied = false;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void ReceiveDamage(int damage){
+		if (damage > health) {
+			health = health - damage;
+		} else {
+			Destroy(this.gameObject);
+		}
+	}
+	
+	//public bool IsOccupied() {
+		//Transform[] children = 
+			//if(this.gameObject.get
+			   //}
+    //}
+
+}

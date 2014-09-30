64432a668c7a37c8c5c41add8c4b98cbd0072aba
kevupton <k_upton@live.com>
2014-09-30 15:36:38 +1000

    adding main menu gui

--- PracticeRun/Assets/Scripts/MainMenuGui.cs
+++ PracticeRun/Assets/Scripts/MainMenuGui.cs
@@ -1,54 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Linq;
 
-public class TroopsGui : MonoBehaviour {
+public class MainMenuGui : MonoBehaviour {
 	const int buttonSpace = 20;
-	const int buttonHeight = 40;
-	const int buttonWidth = 90;
+	const int buttonHeight = 80;
+	const int buttonWidth = 180;
 
 	private ManagerScript managerScript;
-	private bool menuShowing = false;
+	private bool menuShowing = true;
 	private int buttonYPos;
 	private int buttonXPos;
 
-	void Awake () {
-		managerScript = GameObject.FindWithTag ("Manager").GetComponent<ManagerScript> ();
-	}
-
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+	
 	}
 
 	void OnGUI(){
 		if (menuShowing) {
 			DefineScreenWidth ();
-			createButtons();
 		} 
 	}
 
-	public bool MenuShowing () {
-		return menuShowing;
-	}
-
-	private void TroopRequest () {
-
-	}
-
-	private void createButtons () {
-		var taggedObjects = Resources.FindObjectsOfTypeAll(typeof(GameObject)).Cast<GameObject>().Where(g=>g.tag=="troops").ToList();
-
-		GUI.Label (new Rect (buttonXPos + 3, buttonYPos - 25, 200, 30), "PURCHASE TROOPS");
-		foreach (GameObject o in taggedObjects) {
-			
+	void CreateButtons() {
+		if (GUI.Button (new Rect (buttonXPos, buttonYPos, buttonWidth, buttonHeight), "Continue")) {
+			managerScript.currency += 50;
+			HideMenu();
 		}
 		if (GUI.Button (new Rect (buttonXPos, buttonYPos, buttonWidth, buttonHeight), "Add Archers")) {
+			managerScript.currency += 50;
+			HideMenu();
+		}
+		if (GUI.Button (new Rect (buttonXPos, buttonYPos, buttonWidth, buttonHeight), "Exit")) {
 			managerScript.currency += 50;
 			HideMenu();
 		}
@@ -57,7 +45,7 @@ public class TroopsGui : MonoBehaviour {
 	public void HideMenu() {
 		menuShowing = false;
 	}
-
+	
 	public void ShowMenu() {
 		menuShowing = true;
 	}
--- PracticeRun/Assets/Scripts/ManagerScript.cs
+++ PracticeRun/Assets/Scripts/ManagerScript.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class ManagerScript: MonoBehaviour {
 	public int currency = 0;
+	private TroopsGui troopsGui;
+	private MainMenuGui mainMenuGui;
+	private bool paused = false;
 
+	void Awake () {
+		troopsGui = GameObject.FindWithTag ("GUI").GetComponent<TroopsGui> ();
+		mainMenuGui = GameObject.FindWithTag ("GUI").GetComponent<MainMenuGui> ();
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +18,14 @@ public class ManagerScript: MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-
+	void Update () {
+		CheckMainMenu ();
 	}
 
+	private void CheckMainMenu () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			troopsGui.ShowMenu();
+			paused = true;
+		}
+	}
 }
